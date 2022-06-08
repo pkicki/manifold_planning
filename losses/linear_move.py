ff668927ba60efd0901852eb8e047922dec537e8
pkicki <piotr.m.kicki@gmail.com>
2022-06-08 18:13:01 +0200

    Adjusted linear move to new training schemes

--- losses/linear_move.py
+++ losses/linear_move.py
@@ -1,4 +1,5 @@
 from losses.feasibility import FeasibilityLoss
+from losses.utils import huber
 from utils.data import unpack_data_boundaries, unpack_data_linear_move
 import tensorflow as tf
 
@@ -9,6 +10,10 @@ class LinearMoveLoss(FeasibilityLoss):
     def __init__(self, N, urdf_path, q_dot_limits, q_ddot_limits):
         super(LinearMoveLoss, self).__init__(N, urdf_path, q_dot_limits, q_ddot_limits)
         self.man = Iiwa(urdf_path)
+        self.alpha_linear_move = 0.
+        self.alpha_time = 0.
+        self.alpha_q_dot = 0.
+        self.alpha_q_ddot = 0.
 
     def call(self, q_cps, t_cps, data):
         q0, xyz0, xyzk, q_dot_0, q_ddot_0 = unpack_data_linear_move(data, 7)
@@ -21,9 +26,6 @@ class LinearMoveLoss(FeasibilityLoss):
         xyzk = xyzk[..., tf.newaxis]
 
         t_end = 2.
-        #x_range = 0.05
-        #y_range = 0.05
-        #z_range = 0.05
         x_range = tf.linspace(0.1, 0.02, tf.shape(t_cumsum)[-1])[tf.newaxis]
         y_range = tf.linspace(0.1, 0.02, tf.shape(t_cumsum)[-1])[tf.newaxis]
         z_range = tf.linspace(0.1, 0.02, tf.shape(t_cumsum)[-1])[tf.newaxis]
@@ -40,23 +42,40 @@ class LinearMoveLoss(FeasibilityLoss):
         zlow = xyz0[:, 2] - z_range + vz * t_cumsum
         zhigh = xyz0[:, 2] + z_range + vz * t_cumsum
 
-        xlow_loss = tf.nn.relu(xlow - xyz[..., 0, 0])
-        xhigh_loss = tf.nn.relu(xyz[..., 0, 0] - xhigh)
-        ylow_loss = tf.nn.relu(ylow - xyz[..., 1, 0])
-        yhigh_loss = tf.nn.relu(xyz[..., 1, 0] - yhigh)
-        zlow_loss = tf.nn.relu(zlow - xyz[..., 2, 0])
-        zhigh_loss = tf.nn.relu(xyz[..., 2, 0] - zhigh)
-        geometrical_loss = tf.stack([xlow_loss, xhigh_loss, ylow_loss, yhigh_loss, zlow_loss, zhigh_loss], axis=-1)
-        #geometrical_loss = tf.reduce_sum(tf.square(geometrical_loss), axis=-1)
-        geometrical_loss = tf.reduce_sum(geometrical_loss, axis=-1)
-        linear_move_loss = tf.reduce_mean(geometrical_loss, axis=-1)
+        huber_along_path = lambda x: tf.reduce_mean(huber(x), axis=-1)
+        relu_huber_along_path = lambda x: huber_along_path(tf.nn.relu(x))
+        xlow_loss = relu_huber_along_path(xlow - xyz[..., 0, 0])
+        xhigh_loss = relu_huber_along_path(xyz[..., 0, 0] - xhigh)
+        ylow_loss = relu_huber_along_path(ylow - xyz[..., 1, 0])
+        yhigh_loss = relu_huber_along_path(xyz[..., 1, 0] - yhigh)
+        zlow_loss = relu_huber_along_path(zlow - xyz[..., 2, 0])
+        zhigh_loss = relu_huber_along_path(xyz[..., 2, 0] - zhigh)
+        linear_move_loss = tf.stack([xlow_loss, xhigh_loss, ylow_loss, yhigh_loss, zlow_loss, zhigh_loss], axis=-1)
 
-        effort_loss = tf.reduce_mean(tf.reduce_sum(tf.square(q_ddot), axis=-1), axis=-1)
+        time_loss = huber(t - t_end)
 
-        time_loss = tf.square(t - t_end)
+        effort_loss = tf.reduce_mean(huber(q_ddot), axis=-2)
 
-        #base_loss = linear_move_loss + q_dot_loss + q_ddot_loss
-        #model_loss = linear_move_loss + q_dot_loss + q_ddot_loss + effort_loss + time_loss
-        model_loss = linear_move_loss + q_dot_loss + q_ddot_loss + 1e-5 * effort_loss + time_loss
-        #model_loss = tf.where(tf.less(base_loss, 1e-3), model_loss, base_loss + time_loss)
-        return model_loss, linear_move_loss, effort_loss, time_loss, q_dot_loss, q_ddot_loss, q, q_dot, q_ddot, xyz, t, t_cumsum
+        losses = tf.concat([tf.exp(self.alpha_q_dot) * q_dot_loss, tf.exp(self.alpha_q_ddot) * q_ddot_loss,
+                            tf.exp(self.alpha_linear_move) * linear_move_loss, tf.exp(self.alpha_time) * time_loss[:, tf.newaxis],
+                            effort_loss], axis=-1)
+
+        mean_q_dot_loss = tf.reduce_mean(q_dot_loss, axis=-1)
+        mean_q_ddot_loss = tf.reduce_mean(q_ddot_loss, axis=-1)
+        mean_linear_move_loss = tf.reduce_mean(linear_move_loss, axis=-1)
+
+        model_loss = tf.reduce_sum(losses, axis=-1)
+
+        return model_loss, mean_linear_move_loss, effort_loss, time_loss, mean_q_dot_loss, mean_q_ddot_loss, q, q_dot, q_ddot, xyz, t, t_cumsum
+
+
+    def alpha_update(self, q_dot_loss, q_ddot_loss, linear_move_loss, time_loss):
+        gamma = 1e-2
+        alpha_q_dot_update = gamma * (q_dot_loss - 4e-3)
+        alpha_q_ddot_update = gamma * (q_ddot_loss - 3e-2)
+        alpha_linear_move_update = gamma * (linear_move_loss - 1e-5)
+        alpha_time_update = gamma * (time_loss - 1e-3)
+        self.alpha_time += alpha_time_update
+        self.alpha_q_dot += alpha_q_dot_update
+        self.alpha_q_ddot += alpha_q_ddot_update
+        self.alpha_linear_move += alpha_linear_move_update
