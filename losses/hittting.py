603402b47891e07660485ed67d62f756125d2d88
pkicki <piotr.m.kicki@gmail.com>
2022-06-20 13:59:50 +0200

    Test of lagrange method fro calculating lambdas

--- losses/hittting.py
+++ losses/hittting.py
@@ -1,7 +1,12 @@
+from time import perf_counter
+
+from scipy.sparse.linalg import cg
+
 from losses.feasibility import FeasibilityLoss
 from losses.utils import huber
 from utils.manipulator import Iiwa
 import tensorflow as tf
+import numpy as np
 
 
 class HittingLoss(FeasibilityLoss):
@@ -12,10 +17,14 @@ class HittingLoss(FeasibilityLoss):
         self.alpha_constraint = 0.
         self.alpha_q_dot = 0.
         self.alpha_q_ddot = 0.
+        self.lambdas = np.array([1., 1., 1.])
         self.gamma = 1e-2
-        self.bar_constraint = 1e-6
-        self.bar_q_dot = 1e-3
-        self.bar_q_ddot = 1e-2
+        #self.bar_constraint = 1e-6
+        #self.bar_q_dot = 1e-3
+        #self.bar_q_ddot = 1e-2
+        self.bar_constraint = 5e-6
+        self.bar_q_dot = 6e-3
+        self.bar_q_ddot = 6e-2
 
     def call(self, q_cps, t_cps, data):
         _, q_dot_loss, q_ddot_loss, q, q_dot, q_ddot, t, t_cumsum = super().call(q_cps, t_cps, data)
@@ -23,14 +32,27 @@ class HittingLoss(FeasibilityLoss):
         xyz = self.man.forward_kinematics(q)
         constraint_loss = self.end_effector_constraints_distance_function(xyz)
         t_loss = huber(t[:, tf.newaxis])
-        losses = tf.concat([tf.exp(self.alpha_q_dot) * q_dot_loss, tf.exp(self.alpha_q_ddot) * q_ddot_loss,
-                            tf.exp(self.alpha_constraint) * constraint_loss, t_loss], axis=-1)
-        mean_q_dot_loss = tf.reduce_mean(q_dot_loss, axis=-1)
-        mean_q_ddot_loss = tf.reduce_mean(q_ddot_loss, axis=-1)
-        mean_constraint_loss = tf.reduce_mean(constraint_loss, axis=-1)
+        q_dot_loss = tf.reduce_sum(q_dot_loss, axis=-1)
+        q_ddot_loss = tf.reduce_sum(q_ddot_loss, axis=-1)
+        constraint_loss = tf.reduce_sum(constraint_loss, axis=-1)
+        L0 = t_loss
+        L1 = constraint_loss / self.bar_constraint
+        L2 = q_dot_loss / self.bar_q_dot
+        L3 = q_ddot_loss / self.bar_q_ddot
+        Ls = tf.stack([L1, L2, L3], axis=-1)[..., tf.newaxis]
+        model_loss = L0[:, 0] + (self.lambdas[np.newaxis, np.newaxis] @ Ls)[:, 0, 0]
 
-        model_loss = tf.reduce_sum(losses, axis=-1)
-        return model_loss, mean_constraint_loss, mean_q_dot_loss, mean_q_ddot_loss, q, q_dot, q_ddot, xyz, t, t_cumsum
+        #losses = tf.concat([tf.exp(self.alpha_q_dot) * q_dot_loss,
+        #                    tf.exp(self.alpha_q_ddot) * q_ddot_loss,
+        #                    tf.exp(self.alpha_constraint) * constraint_loss,
+        #                    t_loss], axis=-1)
+        #mean_q_dot_loss = tf.reduce_mean(q_dot_loss, axis=-1)
+        #mean_q_ddot_loss = tf.reduce_mean(q_ddot_loss, axis=-1)
+        #mean_constraint_loss = tf.reduce_mean(constraint_loss, axis=-1)
+
+        #model_loss = tf.reduce_sum(losses, axis=-1)
+        return model_loss, constraint_loss, q_dot_loss, q_ddot_loss, q, q_dot, q_ddot, xyz, t, t_cumsum, t_loss
+        #return model_loss, mean_constraint_loss, mean_q_dot_loss, mean_q_ddot_loss, q, q_dot, q_ddot, xyz, t, t_cumsum
 
     def alpha_update(self, q_dot_loss, q_ddot_loss, constraint_loss):
         alpha_q_dot_update = self.gamma * tf.math.log(q_dot_loss / self.bar_q_dot)
@@ -39,3 +61,25 @@ class HittingLoss(FeasibilityLoss):
         self.alpha_q_dot += alpha_q_dot_update
         self.alpha_q_ddot += alpha_q_ddot_update
         self.alpha_constraint += alpha_constraint_update
+
+    def alpha_update_lagrange(self, gL0, gL1, gL2, gL3):
+        A = tf.stack([gL1, gL2, gL3], axis=-1) / tf.stack([self.bar_constraint, self.bar_q_dot, self.bar_q_ddot])[tf.newaxis]
+        b = -gL0[:, tf.newaxis]
+        t0 = perf_counter()
+        #x_pinv = np.linalg.pinv(A) @ b
+        t1 = perf_counter()
+        AT = tf.transpose(A)
+        newA = (AT @ A).numpy()
+        newb = (AT @ b).numpy()
+        t2 = perf_counter()
+        #x = cg(newA, newb)
+        t3 = perf_counter()
+        x_inv = np.linalg.inv(newA) @ newb
+        t4 = perf_counter()
+        ts = [t0, t1, t2, t3, t4]
+        diffs = np.diff(ts)
+        self.lambdas = np.maximum(x_inv[:, 0], 0.)
+        self.alpha_constraint = tf.clip_by_value(tf.math.log(self.lambdas[0] / self.bar_constraint), -1e10, 1e10)
+        self.alpha_q_dot = tf.clip_by_value(tf.math.log(self.lambdas[1] / self.bar_q_dot), -1e10, 1e10)
+        self.alpha_q_ddot = tf.clip_by_value(tf.math.log(self.lambdas[2] / self.bar_q_ddot), -1e10, 1e10)
+        pass
--- train_striker.py
+++ train_striker.py
@@ -20,7 +20,7 @@ from utils.constants import Limits, TableConstraint, UrdfModels
 #config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 class args:
-    batch_size = 64
+    batch_size = 4
     working_dir = './trainings'
     out_name = 'striker_bs64_lr5em5_N15_huberloss_alphatraining'
     log_interval = 10
@@ -57,13 +57,55 @@ for epoch in range(30000):
     constraint_losses = []
     q_dot_losses = []
     q_ddot_losses = []
+    grads_L0 = []
+    grads_L1 = []
+    grads_L2 = []
+    grads_L3 = []
     for i, d in _ds('Train', dataset_epoch, train_size, epoch, args.batch_size):
-        with tf.GradientTape() as tape:
+        with tf.GradientTape(persistent=True) as tape:
             q_cps, t_cps = model(d)
-            model_loss, constraint_loss, q_dot_loss, q_ddot_loss, q, q_dot, q_ddot, xyz, t, t_cumsum = loss(q_cps, t_cps, d)
+            model_loss, constraint_loss, q_dot_loss, q_ddot_loss, q, q_dot, q_ddot, xyz, t, t_cumsum, t_loss = loss(q_cps, t_cps, d)
+            L1 = constraint_loss
+            L2 = q_dot_loss
+            L3 = q_ddot_loss
             z_loss_abs = np.mean(np.abs(xyz[..., -1, 0] - TableConstraint.Z), axis=-1)
         grads = tape.gradient(model_loss, model.trainable_variables)
         opt.apply_gradients(zip(grads, model.trainable_variables))
+        # grad w.r.t. bspline control points
+        #grad_L0 = tape.gradient(t_loss, [t_cps])
+        #grad_L1 = tape.gradient(L1, [q_cps])
+        #grad_L2 = tape.gradient(L2, [q_cps, t_cps])
+        #grad_L3 = tape.gradient(L3, [q_cps, t_cps])
+        #grad_L0 = tf.concat([tf.reshape(tf.zeros_like(grad_L2[0]), (-1, 90)), tf.reshape(grad_L0[0], (-1, 20))], axis=-1)
+        #grad_L1 = tf.concat([tf.reshape(grad_L1[0], (-1, 90)), tf.reshape(tf.zeros_like(grad_L2[1]), (-1, 20))], axis=-1)
+        #grad_L2 = tf.concat([tf.reshape(grad_L2[0], (-1, 90)), tf.reshape(grad_L2[1], (-1, 20))], axis=-1)
+        #grad_L3 = tf.concat([tf.reshape(grad_L3[0], (-1, 90)), tf.reshape(grad_L3[1], (-1, 20))], axis=-1)
+
+        # grad w.r.t. nn params
+        grad_L0 = tape.gradient(t_loss, model.trainable_variables)
+        grad_L1 = tape.gradient(L1, model.trainable_variables)
+        grad_L2 = tape.gradient(L2, model.trainable_variables)
+        grad_L3 = tape.gradient(L3, model.trainable_variables)
+        for i in range(len(grad_L0)):
+            if grad_L0[i] is None:
+                grad_L0[i] = tf.zeros_like(grad_L2[i])
+            if grad_L1[i] is None:
+                grad_L1[i] = tf.zeros_like(grad_L2[i])
+            grad_L0[i] = tf.reshape(grad_L0[i], (-1))
+            grad_L1[i] = tf.reshape(grad_L1[i], (-1))
+            grad_L2[i] = tf.reshape(grad_L2[i], (-1))
+            grad_L3[i] = tf.reshape(grad_L3[i], (-1))
+
+        grad_L0 = tf.concat(grad_L0, axis=0)
+        grad_L1 = tf.concat(grad_L1, axis=0)
+        grad_L2 = tf.concat(grad_L2, axis=0)
+        grad_L3 = tf.concat(grad_L3, axis=0)
+
+
+        grads_L0.append(grad_L0)
+        grads_L1.append(grad_L1)
+        grads_L2.append(grad_L2)
+        grads_L3.append(grad_L3)
 
         constraint_losses.append(constraint_loss)
         q_dot_losses.append(q_dot_loss)
@@ -71,17 +113,26 @@ for epoch in range(30000):
         epoch_loss.append(model_loss)
         with tf.summary.record_if(train_step % args.log_interval == 0):
             tf.summary.scalar('metrics/model_loss', tf.reduce_mean(model_loss), step=train_step)
-            tf.summary.scalar('metrics/constraint_loss', tf.reduce_mean(constraint_loss), step=train_step)
+            tf.summary.scalar('metrics/constraint_loss', tf.reduce_mean(constraint_loss / 5), step=train_step)
             tf.summary.scalar('metrics/z_loss_abs', tf.reduce_mean(z_loss_abs), step=train_step)
-            tf.summary.scalar('metrics/q_dot_loss', tf.reduce_mean(q_dot_loss), step=train_step)
-            tf.summary.scalar('metrics/q_ddot_loss', tf.reduce_mean(q_ddot_loss), step=train_step)
+            tf.summary.scalar('metrics/q_dot_loss', tf.reduce_mean(q_dot_loss / 6), step=train_step)
+            tf.summary.scalar('metrics/q_ddot_loss', tf.reduce_mean(q_ddot_loss / 6), step=train_step)
             tf.summary.scalar('metrics/t', tf.reduce_mean(t), step=train_step)
         train_step += 1
 
-    constraint_losses = tf.reduce_mean(tf.concat(constraint_losses, -1))
-    q_dot_losses = tf.reduce_mean(tf.concat(q_dot_losses, -1))
-    q_ddot_losses = tf.reduce_mean(tf.concat(q_ddot_losses, -1))
-    loss.alpha_update(q_dot_losses, q_ddot_losses, constraint_losses)
+    constraint_losses = tf.reduce_mean(tf.concat(constraint_losses, 0))
+    q_dot_losses = tf.reduce_mean(tf.concat(q_dot_losses, 0))
+    q_ddot_losses = tf.reduce_mean(tf.concat(q_ddot_losses, 0))
+    #loss.alpha_update(q_dot_losses, q_ddot_losses, constraint_losses)
+    grads_L0 = tf.reduce_mean(tf.stack(grads_L0, axis=0), axis=0)
+    grads_L1 = tf.reduce_mean(tf.stack(grads_L1, axis=0), axis=0)
+    grads_L2 = tf.reduce_mean(tf.stack(grads_L2, axis=0), axis=0)
+    grads_L3 = tf.reduce_mean(tf.stack(grads_L3, axis=0), axis=0)
+    #grads_L0 = tf.reduce_mean(tf.concat(grads_L0, axis=0), axis=0)
+    #grads_L1 = tf.reduce_mean(tf.concat(grads_L1, axis=0), axis=0)
+    #grads_L2 = tf.reduce_mean(tf.concat(grads_L2, axis=0), axis=0)
+    #grads_L3 = tf.reduce_mean(tf.concat(grads_L3, axis=0), axis=0)
+    loss.alpha_update_lagrange(grads_L0, grads_L1, grads_L2, grads_L3)
 
 
 
@@ -107,16 +158,16 @@ for epoch in range(30000):
     experiment_handler.log_validation()
     for i, d in _ds('Val', dataset_epoch, val_size, epoch, args.batch_size):
         q_cps, t_cps = model(d)
-        model_loss, constraint_loss, q_dot_loss, q_ddot_loss, q, q_dot, q_ddot, xyz, t, t_cumsum = loss(q_cps, t_cps, d)
+        model_loss, constraint_loss, q_dot_loss, q_ddot_loss, q, q_dot, q_ddot, xyz, t, t_cumsum, t_loss = loss(q_cps, t_cps, d)
         z_loss_abs = np.mean(np.abs(xyz[..., -1, 0] - TableConstraint.Z), axis=-1)
 
         epoch_loss.append(model_loss)
         with tf.summary.record_if(val_step % args.log_interval == 0):
             tf.summary.scalar('metrics/model_loss', tf.reduce_mean(model_loss), step=val_step)
-            tf.summary.scalar('metrics/constraint_loss', tf.reduce_mean(constraint_loss), step=val_step)
+            tf.summary.scalar('metrics/constraint_loss', tf.reduce_mean(constraint_loss / 5), step=val_step)
             tf.summary.scalar('metrics/z_loss_abs', tf.reduce_mean(z_loss_abs), step=val_step)
-            tf.summary.scalar('metrics/q_dot_loss', tf.reduce_mean(q_dot_loss), step=val_step)
-            tf.summary.scalar('metrics/q_ddot_loss', tf.reduce_mean(q_ddot_loss), step=val_step)
+            tf.summary.scalar('metrics/q_dot_loss', tf.reduce_mean(q_dot_loss / 6), step=val_step)
+            tf.summary.scalar('metrics/q_ddot_loss', tf.reduce_mean(q_ddot_loss / 6), step=val_step)
             tf.summary.scalar('metrics/t', tf.reduce_mean(t), step=val_step)
         val_step += 1
 
