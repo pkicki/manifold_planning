ab37795dcd82113b6cfef8276ae536483144c261
pkicki <piotr.m.kicki@gmail.com>
2022-06-24 15:04:42 +0200

    Integration issues fixed

--- utils/model.py
+++ utils/model.py
@@ -33,7 +33,7 @@ def load_model_hpo(path):
     return model
 
 
-def model_inference(model, data, bsp, bspt):
+def model_inference(model, data, bsp, bspt, uniform=False, freq=100):
     q_cps, t_cps = model(data)
     q = bsp.N @ q_cps
     q_dot_tau = bsp.dN @ q_cps
@@ -48,26 +48,27 @@ def model_inference(model, data, bsp, bspt):
     ts = 1. / dtau_dt[..., 0] / 1024.
     t = tf.cumsum(np.concatenate([np.zeros_like(ts[..., :1]), ts[..., :-1]], axis=-1), axis=-1)
 
-    t = np.concatenate([np.zeros((1, 1)), t[:, :-1]], axis=-1)[0]
-    si = interp1d(t, np.linspace(0., 1., 1024), axis=-1)
-    n = 100
-    targ = np.linspace(t[0], t[-1], n)
-    s = si(targ)
+    if uniform:
+        t = t[0]
+        si = interp1d(t, np.linspace(0., 1., 1024), axis=-1)
+        targ = np.linspace(t[0], t[-1], int(t[-1] * freq))
+        s = si(targ)
 
-    dtau_dt_bs = BSp(bspt.u, t_cps[0, :, 0], 7)
-    ddtau_dtt_bs = dtau_dt_bs.derivative()
-    q_bs = BSp(bsp.u, q_cps[0, :], 7)
-    dq_bs = q_bs.derivative()
-    ddq_bs = dq_bs.derivative()
+        dtau_dt_bs = BSp(bspt.u, t_cps[0, :, 0], 7)
+        ddtau_dtt_bs = dtau_dt_bs.derivative()
+        q_bs = BSp(bsp.u, q_cps[0, :], 7)
+        dq_bs = q_bs.derivative()
+        ddq_bs = dq_bs.derivative()
 
-    q = q_bs(s)
-    q_dot_tau = dq_bs(s)
-    q_ddot_tau = ddq_bs(s)
-    dtau_dt = dtau_dt_bs(s)[..., np.newaxis]
-    ddtau_dtt = ddtau_dtt_bs(s)[..., np.newaxis]
-    t = targ
+        q = q_bs(s)
+        q_dot_tau = dq_bs(s)
+        q_ddot_tau = ddq_bs(s)
+        dtau_dt = dtau_dt_bs(s)[..., np.newaxis]
+        ddtau_dtt = ddtau_dtt_bs(s)[..., np.newaxis]
+        t = targ
 
-    q_dot = q_dot_tau * dtau_dt
-    q_ddot = q_ddot_tau * dtau_dt ** 2 + ddtau_dtt * q_dot_tau * dtau_dt
+        q_dot = q_dot_tau * dtau_dt
+        q_ddot = q_ddot_tau * dtau_dt ** 2 + ddtau_dtt * q_dot_tau * dtau_dt
 
-    return q, q_dot, q_ddot, t
+        return q, q_dot, q_ddot, t
+    return q.numpy()[0], q_dot.numpy()[0], q_ddot.numpy()[0], t.numpy()[0]
