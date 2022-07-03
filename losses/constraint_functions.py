22423a632a3c5a8e6a627018ea8fbb65896559b4
pkicki <piotr.m.kicki@gmail.com>
2022-07-03 14:31:30 +0200

    Updated constrain function to consider displacement while violating constraints

--- losses/constraint_functions.py
+++ losses/constraint_functions.py
@@ -6,12 +6,15 @@ table = Table()
 
 
 def air_hockey_table(xyz):
-    huber_along_path = lambda x: tf.reduce_mean(huber(x), axis=-1)
+    xyz = xyz[..., 0]
+    v = tf.linalg.norm(xyz[:, 1:] - xyz[:, :-1], axis=-1)
+    xyz = xyz[:, 1:]
+    huber_along_path = lambda x: tf.reduce_sum(v * huber(x), axis=-1)
     relu_huber_along_path = lambda x: huber_along_path(tf.nn.relu(x))
-    xlow_loss = relu_huber_along_path(table.xlb - xyz[..., 0, 0])
-    xhigh_loss = relu_huber_along_path(xyz[..., 0, 0] - table.xrt)
-    ylow_loss = relu_huber_along_path(table.ylb - xyz[..., 1, 0])
-    yhigh_loss = relu_huber_along_path(xyz[..., 1, 0] - table.yrt)
-    z_loss = huber_along_path(xyz[..., 2, 0] - table.z)
+    xlow_loss = relu_huber_along_path(table.xlb - xyz[..., 0])
+    xhigh_loss = relu_huber_along_path(xyz[..., 0] - table.xrt)
+    ylow_loss = relu_huber_along_path(table.ylb - xyz[..., 1])
+    yhigh_loss = relu_huber_along_path(xyz[..., 1] - table.yrt)
+    z_loss = huber_along_path(xyz[..., 2] - table.z)
     constraint_losses = tf.stack([xlow_loss, xhigh_loss, ylow_loss, yhigh_loss, z_loss], axis=-1)
     return constraint_losses
