241ab2130a39ce93ee398d50dc4d9c49ea2d7026
pkicki <piotr.m.kicki@gmail.com>
2022-07-10 22:15:08 +0200

    Polynomial expected time approximation

--- models/iiwa_planner_boundaries.py
+++ models/iiwa_planner_boundaries.py
@@ -2,6 +2,7 @@ from math import pi
 
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 from utils.constants import Limits
 from utils.data import unpack_data_boundaries
@@ -43,6 +44,41 @@ class IiwaPlannerBoundaries(tf.keras.Model):
         q0, qd, xyth, q_dot_0, q_ddot_0, q_dot_d = unpack_data_boundaries(x, self.n_dof + 1)
 
         expected_time = tf.reduce_max(tf.abs(qd - q0) / Limits.q_dot[np.newaxis], axis=-1)
+        a_0 = q0[:, tf.newaxis]
+        a_1 = (q_dot_0 + 3 * q0)[:, tf.newaxis]
+        a_3 = qd[:, tf.newaxis]
+        a_2 = (3 * qd - q_dot_d)[:, tf.newaxis]
+        t = tf.linspace(0., 1., 128)[tf.newaxis, :, tf.newaxis]
+        #q_ = a_3 * t ** 3 + a_2 * t ** 2 * (1 - t) + a_1 * t * (1 - t) ** 2 + a_0 * (1 - t) ** 3
+        q_dot_ = 3 * a_3 * t ** 2 + a_2 * (-3 * t ** 2 + 2 * t) + a_1 * (
+                3 * t ** 2 - 4 * t + 1) - a_0 * 3 * (1 - t) ** 2
+        q_ddot_ = 6 * a_3 * t ** 1 + a_2 * (-6 * t + 2) +\
+                  a_1 * (6 * t - 4) + a_0 * 6 * (1 - t)
+
+        q_dot_mul = tf.reduce_max(tf.abs(q_dot_) / Limits.q_dot[np.newaxis, np.newaxis], axis=-1)
+        q_ddot_mul = tf.reduce_max(tf.abs(q_ddot_) / Limits.q_ddot[np.newaxis, np.newaxis], axis=-1)
+
+        exp_t_q_dot = tf.reduce_mean(q_dot_mul, axis=-1)
+        exp_t_q_ddot = tf.reduce_mean(tf.sqrt(q_ddot_mul), axis=-1)
+        #q_dot_mul = tf.reduce_max(tf.abs(q_dot_) / Limits.q_dot[np.newaxis, np.newaxis], axis=(-1, -2))
+        #q_ddot_mul = tf.reduce_max(tf.abs(q_ddot_) / Limits.q_ddot[np.newaxis, np.newaxis], axis=(-1, -2))
+
+        #exp_t_q_dot = q_dot_mul
+        #exp_t_q_ddot = tf.sqrt(q_ddot_mul)
+        expected_time_ = tf.maximum(exp_t_q_dot, exp_t_q_ddot)
+
+        #print(q_dot_mul[0])
+        #print(q_ddot_mul[0])
+        #print(exp_t_q_dot[0])
+        #print(exp_t_q_ddot[0])
+        #print(expected_time[0])
+        #for i in range(6):
+        #    plt.subplot(231 + i)
+        #    plt.plot(q_[0, :, i], label="q")
+        #    plt.plot(q_dot_[0, :, i], label="dq")
+        #    plt.plot(q_ddot_[0, :, i], label="ddq")
+        #    plt.legend()
+        #plt.show()
 
         xb = q0 / pi
         if self.n_pts_fixed_begin > 1:
@@ -66,7 +102,7 @@ class IiwaPlannerBoundaries(tf.keras.Model):
         for l in self.t_est:
             dtau_dt = l(dtau_dt)
 
-        dtau_dt = dtau_dt / expected_time[:, tf.newaxis]
+        dtau_dt = dtau_dt / expected_time_[:, tf.newaxis]
 
         q = pi * tf.reshape(q_est, (-1, self.N, self.n_dof))
         s = tf.linspace(0., 1., tf.shape(q)[1] + 2)[tf.newaxis, 1:-1, tf.newaxis]
