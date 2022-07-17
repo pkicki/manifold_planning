Fixed torque loss calculation

losses/feasibility.py
@@ -10,12 +10,13 @@ from utils.manipulator import Iiwa
 
 
 class FeasibilityLoss:
-    def __init__(self, N, urdf_path, q_dot_limits, q_ddot_limits, q_dddot_limits):
+    def __init__(self, N, urdf_path, q_dot_limits, q_ddot_limits, q_dddot_limits, torque_limits):
         self.bsp_t = BSpline(20)
         self.bsp = BSpline(N)
         self.q_dot_limits = q_dot_limits
         self.q_ddot_limits = q_ddot_limits
         self.q_dddot_limits = q_dddot_limits
+        self.torque_limits = torque_limits
         urdf_path = UrdfModels.iiwa
         self.iiwa = Iiwa(urdf_path)
         self.model = pino.buildModelFromUrdf(urdf_path)
@@ -73,7 +74,7 @@ class FeasibilityLoss:
         q_dot_limits = tf.constant(self.q_dot_limits)[tf.newaxis, tf.newaxis]
         q_ddot_limits = tf.constant(self.q_ddot_limits)[tf.newaxis, tf.newaxis]
         q_dddot_limits = tf.constant(self.q_dddot_limits)[tf.newaxis, tf.newaxis]
-        torque_limits = tf.constant(self.q_ddot_limits)[tf.newaxis, tf.newaxis]
+        torque_limits = tf.constant(self.torque_limits)[tf.newaxis, tf.newaxis]
 
         #torque = self.rnea(q, q_dot, q_ddot)
         torque = self.iiwa.rnea(q, q_dot, q_ddot)[..., :6]

losses/hittting.py
@@ -11,8 +11,9 @@ import numpy as np
 
 
 class HittingLoss(FeasibilityLoss):
-    def __init__(self, N, urdf_path, end_effector_constraint_distance_function, q_dot_limits, q_ddot_limits, q_dddot_limits):
-        super(HittingLoss, self).__init__(N, urdf_path, q_dot_limits, q_ddot_limits, q_dddot_limits)
+    def __init__(self, N, urdf_path, end_effector_constraint_distance_function, q_dot_limits, q_ddot_limits,
+                 q_dddot_limits, torque_limits):
+        super(HittingLoss, self).__init__(N, urdf_path, q_dot_limits, q_ddot_limits, q_dddot_limits, torque_limits)
         self.end_effector_constraints_distance_function = end_effector_constraint_distance_function
         self.man = Iiwa(urdf_path)
         self.alpha_constraint = tf.math.log(1e-2)

train_striker.py
@@ -44,7 +44,7 @@ urdf_path = os.path.join(os.path.dirname(__file__), UrdfModels.striker)
 
 N = 15
 opt = tf.keras.optimizers.Adam(args.learning_rate)
-loss = HittingLoss(N, urdf_path, air_hockey_table, Limits.q_dot, Limits.q_ddot, Limits.q_dddot)
+loss = HittingLoss(N, urdf_path, air_hockey_table, Limits.q_dot, Limits.q_ddot, Limits.q_dddot, Limits.tau)
 model = IiwaPlannerBoundaries(N, 3, 2, loss.bsp, loss.bsp_t)
 
 experiment_handler = ExperimentHandler(args.working_dir, args.out_name, args.log_interval, model, opt)
@@ -113,15 +113,15 @@ for epoch in range(30000):
         tf.summary.scalar('epoch/alpha_constraint', loss.alpha_constraint, step=epoch)
         tf.summary.scalar('epoch/alpha_torque', loss.alpha_torque, step=epoch)
 
-    w = 1
-    if epoch % w == w - 1:
-       plot_qs(epoch, q, q_dot, q_ddot, dt, t_cumsum)
-       plt.plot(xyz[0, ..., -1, 0])
-       plt.savefig(f"z_{epoch:05d}.png")
-       plt.clf()
-       plt.plot(xyz[0, ..., 0, 0], xyz[0, ..., 1, 0])
-       plt.savefig(f"xy_{epoch:05d}.png")
-       plt.clf()
+    #w = 1
+    #if epoch % w == w - 1:
+    #   plot_qs(epoch, q, q_dot, q_ddot, dt, t_cumsum)
+    #   plt.plot(xyz[0, ..., -1, 0])
+    #   plt.savefig(f"z_{epoch:05d}.png")
+    #   plt.clf()
+    #   plt.plot(xyz[0, ..., 0, 0], xyz[0, ..., 1, 0])
+    #   plt.savefig(f"xy_{epoch:05d}.png")
+    #   plt.clf()
 
     # validation
     dataset_epoch = val_ds.shuffle(val_size)