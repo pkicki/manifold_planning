dcb9acb8427d629da35d0205a290fb7bba664325
pkicki <piotr.m.kicki@gmail.com>
2022-08-06 18:09:36 +0200

    Data generation

--- data/generate_data_iiwa_hitting.py
+++ data/generate_data_iiwa_hitting.py
@@ -8,18 +8,14 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 sys.path.insert(0, os.path.dirname(parentdir))
-#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 import numpy as np
-#import tensorflow as tf
 from utils.constants import TableConstraint, Limits, UrdfModels, Base
-
-#from models.iiwa_ik_hitting import IiwaIKHitting
-#from utils.execution import ExperimentHandler
-
 from utils.spo import StartPointOptimizer
 from utils.velocity_projection import VelocityProjector
 from utils.hpo_interface import get_hitting_configuration_opt
+from data.generate_data_iiwa_universal_planner import get_hitting_configuration
 
 
 def generate_hitting_directions(x, y):
@@ -98,76 +94,50 @@ def validate_if_pose_is_reachable_with_given_velocity(x, y, v_x, v_y):
 
 def validate_if_initial_mallet_and_puck_positions_makes_hit_possible(xm, ym, xp, yp):
     """Validates if given initial mallet and puck positions enables one to plan reasonable movement"""
-    dist = np.sqrt((ym - yp) ** 2 + (xm - xp) ** 2) > 0.15
-    #x = xp > xm
-    return dist# and x
-
-
-urdf_path = os.path.join(os.path.dirname(__file__), "..", UrdfModels.striker)
-po = StartPointOptimizer(urdf_path)
-vp = VelocityProjector(urdf_path)
-
-pino_model = pino.buildModelFromUrdf(urdf_path)
-pino_data = pino_model.createData()
-
-#opt = tf.keras.optimizers.Adam(1e0)
-#model = IiwaIKHitting()
-#experiment_handler = ExperimentHandler("./trainings", "test", 1, model, opt)
-#experiment_handler.restore(f"../trainings/velpos/porthos_pos/last_n-20")
-#experiment_handler.restore(f"../trained_models/ik_hitting/pos/best-104")
-#experiment_handler.restore(f"../trained_models/ik_hitting/pos_lossabs/best-77")
-#experiment_handler.restore(f"../trained_models/ik_hitting/pos_lossabs/best-23")
-#experiment_handler.restore(f"../trained_models/ik_hitting/pos_lossabs/best-44")
-# experiment_handler.restore(f"../trained_models/ik_hitting/pos_lossabs/best-93")
-#experiment_handler.restore(f"../trained_models/ik_hitting/pos_lossabs/best-53") # beyond adas1
-
-
-def get_hitting_configuration(xk, yk, thk):
-    #qk = model(np.array([xk, yk, thk])[np.newaxis])
-    #q = np.concatenate([qk.numpy()[0], np.zeros(3)], axis=-1)
-    qk, q_dot_k = get_hitting_configuration_opt(xk, yk, Base.position[-1], thk)
-    if qk is None or q_dot_k is None:
-        return None, None
-    q = np.concatenate([qk, np.zeros(2)], axis=-1)
-    pino.forwardKinematics(pino_model, pino_data, q)
-    xyz_pino = pino_data.oMi[-1].translation
-    idx_ = pino_model.getFrameId("F_striker_tip")
-    J_36 = pino.computeFrameJacobian(pino_model, pino_data, q, idx_, pino.LOCAL_WORLD_ALIGNED)[:3, :6]
-    v = J_36 @ np.array(q_dot_k[:6])
-    return q[:7].tolist(), q_dot_k[:6], v
-    #idx_ = pino_model.getFrameId("F_striker_tip")
-    #J = pino.computeFrameJacobian(pino_model, pino_data, q, idx_, pino.LOCAL_WORLD_ALIGNED)[:3, :6]
-    ##J = pino.computeJointJacobians(pino_model, pino_data, q)
-    #pinvJ = np.linalg.pinv(J)
-    #q_dot = (pinvJ @ np.array([np.cos(thk), np.sin(thk), vz])[:, np.newaxis])[:, 0]
-    #max_mul = np.max(np.abs(q_dot) / Limits.q_dot)
-    #qdotk = q_dot / max_mul
-    #err = np.abs(xyz_pino - np.array([xk, yk, TableConstraint.Z]))
-    #return q[:7].tolist(), qdotk.tolist()
-
+    dist = np.sqrt((ym - yp) ** 2 + (xm - xp) ** 2) > 0.1
+    x = xp > xm
+    return dist and x
 
 if __name__ == "__main__":
+    urdf_path = os.path.join(os.path.dirname(__file__), "..", UrdfModels.striker)
+    po = StartPointOptimizer(urdf_path)
+    vp = VelocityProjector(urdf_path)
+
+    pino_model = pino.buildModelFromUrdf(urdf_path)
+    pino_data = pino_model.createData()
+
+
+    #def get_hitting_configuration(xk, yk, thk):
+    #    qk, q_dot_k = get_hitting_configuration_opt(xk, yk, Base.position[-1], thk)
+    #    if qk is None or q_dot_k is None:
+    #        return None, None
+    #    q = np.concatenate([qk, np.zeros(2)], axis=-1)
+    #    pino.forwardKinematics(pino_model, pino_data, q)
+    #    xyz_pino = pino_data.oMi[-1].translation
+    #    return q[:7].tolist(), q_dot_k[:6]
+
+
     data = []
     ds = sys.argv[1]
     assert ds in ["train", "val", "test", "dummy"]
     idx = int(sys.argv[2])
     N = int(sys.argv[3])
 
-    x0l = 0.6
-    x0h = 1.4
-    y0l = -0.45
-    y0h = 0.45
-    xkl = 0.6
-    xkh = 1.4
+    x0l = 0.60
+    x0h = 0.7
+    y0l = -0.05
+    y0h = 0.05
+    xkl = 0.65
+    xkh = 1.3
     ykl = -0.45
     ykh = 0.45
     i = 0
-    qdot_violation = 1.2
-    qddot_violation = 1.2
+    qdot_violation = 1.0
+    qddot_violation = 1.0
     while i < N:
         x0 = (x0h - x0l) * np.random.rand() + x0l
         y0 = (y0h - y0l) * np.random.rand() + y0l
-        dz0 = 0.01 * (2*np.random.rand() - 1.)
+        dz0 = 0.005 * (2*np.random.rand() - 1.)
         th0 = np.pi * (2 * np.random.random() - 1.)
 
         xk = (xkh - xkl) * np.random.rand() + xkl
@@ -184,46 +154,29 @@ if __name__ == "__main__":
 
         point = np.array([x0, y0, TableConstraint.Z+dz0])
         q0 = po.solve(point)
-        alpha = 2 * np.random.random(3) - 1.
-        q_dot_0 = vp.compute_q_dot(q0, v_xyz, alpha)[:7]
+        q_dot_0 = np.zeros((6))
+        q_ddot_0 = np.zeros((6))
 
-        dth = 0.02 * (2 * np.random.random() - 1)
-        v_xy_ = (0.1*np.random.random() + 0.95) * np.array([np.cos(th0+dth), np.sin(th0+dth)])
-        v_z_ = v_z + 0.02 * (2 * np.random.random(1) - 1)
-        v_xyz_ = np.concatenate([v_xy_, v_z_, np.zeros(3)])
-        alpha_ = alpha + 0.2 * (2 * np.random.random(3) - 1.)
-        q_dot_0_ = vp.compute_q_dot(q0, v_xyz_, alpha_)[:7]
-        q_ddot_0 = (q_dot_0_ - q_dot_0)[:6]
-        q_ddot_0_mul = np.min(Limits.q_ddot / np.abs(q_ddot_0))
-        q_ddot_0 = np.random.random() * q_ddot_0_mul * q_ddot_0 * qddot_violation
-
-        ql = Limits.q_dot
-        max_gain = np.min(ql / np.abs(q_dot_0[:6]))
-        q_dot_mul = max_gain * np.random.random()
-        if np.random.random() < 0.2:
-            q_dot_mul = 0.
-        q_dot_0 = q_dot_mul * q_dot_0 * qdot_violation
-
-        if not validate_if_pose_is_reachable_with_given_velocity(x0, y0, v_xy[0] * q_dot_mul, v_xy[1] * q_dot_mul):
-            continue
-
-        #thk = np.pi / 2 * (2 * np.random.random() - 1.)
-        #thk = np.pi/2 - 0.1
-        thk = np.pi * (2 * np.random.random() - 1.)
-        #xg = 2.49
-        #yg = 0.
+        xg = 2.49
+        yg = 0.
         # Direction 1: straight
-        #thk = np.arctan2(yg - yk, xg - xk) + 0.3 * (2*np.random.random() - 1.)
+        thk = np.arctan2(yg - yk, xg - xk) + 0.3 * (2*np.random.random() - 1.)
         qk, q_dot_k, vk = get_hitting_configuration(xk, yk, thk)
         if qk is None or q_dot_k is None:
             continue
         q_dot_k = np.array(q_dot_k)
+        ql = Limits.q_dot
         max_gain = np.min(ql / np.abs(q_dot_k[:6]))
         q_dot_mul = max_gain * np.random.random()
         if np.random.random() < 0.5:
             q_dot_mul = max_gain
         q_dot_k = q_dot_mul * q_dot_k
 
+        #dth = 0.02 * (2 * np.random.random() - 1)
+        ##vz = 0.02 * np.random.random()
+        #dx = 0.02 * np.cos(thk + dth)
+        #dy = 0.02 * np.sin(thk + dth)
+        #_, q_dot_k_ = get_hitting_configuration(xk + dx, yk + dy, thk + dth)
         vk_angle = np.arctan2(vk[1], vk[0]) + (0.2 * np.random.random() - 0.1)
         vk_mag = np.linalg.norm(vk) * (0.1 * np.random.random() + 0.95)
         vk_ = vk_mag * np.array([np.cos(vk_angle), np.sin(vk_angle), 0.01 * (2 * np.random.random() - 1.)])
@@ -231,7 +184,6 @@ if __name__ == "__main__":
         q_dot_k_ = vp.compute_q_dot(np.array(qk), vk_, alpha)[:6]
         if q_dot_k_ is None:
             continue
-        q_dot_k_ = q_dot_mul * np.array(q_dot_k_)
         q_ddot_k = (np.array(q_dot_k_) - q_dot_k)
         q_ddot_k_mul = np.min(Limits.q_ddot / np.abs(q_ddot_k[:6]))
         q_ddot_k = np.random.random() * q_ddot_k_mul * q_ddot_k * qddot_violation
@@ -247,7 +199,8 @@ if __name__ == "__main__":
         i += 1
 
     # dir_name = f"paper/airhockey_table_moves_v08_a10v_tilted_93/{ds}"
-    dir_name = f"paper/airhockey_table_moves_v08_a10v_optimized_regularized_man_omega_nolp/{ds}"
+    #dir_name = f"paper/airhockey_table_moves_v08_a10v_optimized_hitting_2/{ds}"
+    dir_name = f"paper/airhockey_table_moves_v08_a10v_optimized_hitting_regularized_man_lp/{ds}"
     ranges = [x0l, x0h, y0l, y0h, xkl, xkh, ykl, ykh]
     os.makedirs(dir_name, exist_ok=True)
     np.savetxt(f"{dir_name}/data_{N}_{idx}.tsv", data, delimiter='\t', fmt="%.8f")
