c3d94406a1f3c9d912aea2a652deff5eaba660d7
pkicki <piotr.m.kicki@gmail.com>
2022-08-13 11:14:32 +0200

    Start point optimization with initial condition

--- utils/spo.py
+++ utils/spo.py
@@ -13,10 +13,13 @@ class StartPointOptimizer:
         self.n = n
         self.model = pino.buildModelFromUrdf(urdf_path)
         self.data = self.model.createData()
-        self.bounds = spo.Bounds(self.model.lowerPositionLimit[:7], self.model.upperPositionLimit[:7])
+        self.bounds = spo.Bounds(self.model.lowerPositionLimit[:6], self.model.upperPositionLimit[:6])
 
-    def solve(self, point):
-        x0 = np.array(Base.configuration + [0.])
+    def solve(self, point, q0=None):
+        if q0 is None:
+            #q0 = np.array(Base.configuration + [0.])
+            q0 = Base.configuration
+        #q0 = np.pad(q0, [[0, 7 - len(q0)]])
         #x0 = np.array([0., 0.7135205165808098, 0., -0.5024774869152212, 0., 1.9256622406212651, 0.])
         #x0 = np.array([0.0, 0.06580,  0.0, -1.45996, 0.,  1.22487, 0.0])
         #x0 = np.array([np.pi/3 * (2*np.random.random() - 1.),
@@ -29,12 +32,12 @@ class StartPointOptimizer:
         options = {'maxiter': 300, 'ftol': 1e-06, 'iprint': 1, 'disp': False,
                    'eps': 1.4901161193847656e-08, 'finite_diff_rel_step': None}
         t0 = time()
-        r = spo.minimize(lambda x: self.f(x, point), x0, method='SLSQP',
+        r = spo.minimize(lambda x: self.f(x, point), q0, method='SLSQP',
                          bounds=self.bounds, options=options)
         t1 = time()
         print(r)
         print("TIME:", t1 - t0)
-        return r.x
+        return np.pad(r.x, [[0, 1]])
 
     def f(self, q, hit_point):
         pino.forwardKinematics(self.model, self.data, np.pad(q, (0, self.n - len(q)), mode='constant'))
